14b5dfdad06e202fd41efc56fe8563a251536f5d
iBlaze <58302540+Toxic-Omega@users.noreply.github.com>
2021-09-28 05:49:04 -0700

    Add files via upload

---
+++ psc.py
@@ -0,0 +1,26 @@
+import re
+import os
+import time
+import colorama
+
+print("""
+__________  __________________  
+\______   \/   _____/\_   ___ \ 
+ |     ___/\_____  \ /    \  \/ 
+ |    |    /        \\     \____
+ |____|   /_______  / \______  /
+                  \/         \/ 
+
+    Per Second Calculator
+""")
+second = int(input("Enter Number : "))
+print("")
+minute = second * 60
+hour = minute * 60
+tenhours = hour * 10
+day = hour * 24
+print(re.sub(r'(?<!^)(?=(\d{3})+$)', r'.', "Per Second : %d" %second))
+print(re.sub(r'(?<!^)(?=(\d{3})+$)', r'.', "Per Minute : %d" %minute))
+print(re.sub(r'(?<!^)(?=(\d{3})+$)', r'.', "Per Hour : %d" %hour))
+print(re.sub(r'(?<!^)(?=(\d{3})+$)', r'.', "Per 10 Hours : %d" %tenhours))
+print(re.sub(r'(?<!^)(?=(\d{3})+$)', r'.', "Per Day : %d" %day))
